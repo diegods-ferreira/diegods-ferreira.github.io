13773cc9473b0e5c9f03b01557092a234334845f
Diego Ferreira <diegosf1705@gmail.com>
2022-07-30 12:02:18 -0300

    Implemented animation on nav bar elements

--- src/components/nav-bar/nav-bar.component.tsx
+++ src/components/nav-bar/nav-bar.component.tsx
@@ -1,37 +1,43 @@
 import React from 'react';
 import { FiMenu, FiX } from 'react-icons/fi';
-import { Box, Flex, IconButton, Stack, Collapse, Icon, useDisclosure, Avatar } from '@chakra-ui/react';
+import { Box, Flex, IconButton, Stack, Collapse, Icon, useDisclosure, Avatar, ScaleFade } from '@chakra-ui/react';
 
 import { NavItem, NavItemHref, NavLink } from './nav-link.component';
 
-interface NavBarProps {
+interface GeneralNavBarProps {
   navItems: NavItem[];
   activeMenu: NavItemHref;
-  // eslint-disable-next-line react/no-unused-prop-types
+}
+
+interface NavBarProps {
   elmentRef?: any;
 }
 
-const DesktopNav: React.FC<NavBarProps> = ({ navItems, activeMenu }) => {
+interface MobileNavProps {
+  isOpen: boolean;
+}
+
+const DesktopNav: React.FC<GeneralNavBarProps> = ({ navItems, activeMenu }) => {
   return (
     <Stack direction="row" spacing={4}>
-      {navItems.map((item) => (
-        <NavLink key={item.href} link={item} isActive={activeMenu === item.href} />
+      {navItems.map((item, index) => (
+        <NavLink key={item.href} link={item} isActive={activeMenu === item.href} inView delay={index * 0.2} />
       ))}
     </Stack>
   );
 };
 
-const MobileNav: React.FC<NavBarProps> = ({ navItems, activeMenu }) => {
+const MobileNav: React.FC<GeneralNavBarProps & MobileNavProps> = ({ navItems, activeMenu, isOpen }) => {
   return (
     <Stack bg="white" borderTop="1px" borderStyle="solid" borderColor="gray.200" p="16px" display={{ md: 'none' }}>
-      {navItems.map((item) => (
-        <NavLink key={item.href} link={item} isActive={activeMenu === item.href} />
+      {navItems.map((item, index) => (
+        <NavLink key={item.href} link={item} isActive={activeMenu === item.href} inView={isOpen} delay={index * 0.1} />
       ))}
     </Stack>
   );
 };
 
-export const NavBar: React.FC<NavBarProps> = ({ navItems, activeMenu, elmentRef }) => {
+export const NavBar: React.FC<GeneralNavBarProps & NavBarProps> = ({ navItems, activeMenu, elmentRef }) => {
   const { isOpen, onToggle } = useDisclosure();
 
   return (
@@ -57,22 +63,26 @@ export const NavBar: React.FC<NavBarProps> = ({ navItems, activeMenu, elmentRef 
         align="center"
       >
         <Flex flex={{ base: 1, md: 'auto' }} ml={{ base: -2 }} display={{ base: 'flex', md: 'none' }}>
-          <IconButton
-            onClick={onToggle}
-            icon={<Icon as={isOpen ? FiX : FiMenu} w={5} h={5} />}
-            variant="ghost"
-            colorScheme="textSecondary"
-            aria-label="Toggle Navigation"
-          />
+          <ScaleFade initialScale={0} in transition={{ enter: { duration: 0.5 } }}>
+            <IconButton
+              onClick={onToggle}
+              icon={<Icon as={isOpen ? FiX : FiMenu} w={5} h={5} />}
+              variant="ghost"
+              colorScheme="textSecondary"
+              aria-label="Toggle Navigation"
+            />
+          </ScaleFade>
         </Flex>
 
         <Flex flex={{ base: 1 }} justify={{ base: 'flex-end', md: 'start' }}>
-          <Avatar
-            name="Diego Ferreira"
-            bgGradient="linear(to-r, peachRed.500, peachOrange.500)"
-            fontFamily="Montserrat"
-            fontWeight="bold"
-          />
+          <ScaleFade initialScale={0} in transition={{ enter: { duration: 0.5 } }}>
+            <Avatar
+              name="Diego Ferreira"
+              bgGradient="linear(to-r, peachRed.500, peachOrange.500)"
+              fontFamily="Montserrat"
+              fontWeight="bold"
+            />
+          </ScaleFade>
 
           <Flex flex={{ base: 1 }} display={{ base: 'none', md: 'flex' }} justifyContent="flex-end">
             <DesktopNav navItems={navItems} activeMenu={activeMenu} />
@@ -80,8 +90,8 @@ export const NavBar: React.FC<NavBarProps> = ({ navItems, activeMenu, elmentRef 
         </Flex>
       </Flex>
 
-      <Collapse in={isOpen} animateOpacity>
-        <MobileNav navItems={navItems} activeMenu={activeMenu} />
+      <Collapse in={isOpen}>
+        <MobileNav navItems={navItems} activeMenu={activeMenu} isOpen={isOpen} />
       </Collapse>
     </Box>
   );
--- src/components/nav-bar/nav-link.component.tsx
+++ src/components/nav-bar/nav-link.component.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Center, Link } from '@chakra-ui/react';
+import { Box, Center, Link, SlideFade } from '@chakra-ui/react';
 
 export type NavItemHref = 'home' | 'about' | 'projects' | 'contact';
 
@@ -12,44 +12,48 @@ export interface NavItem {
 interface NavLinkProps {
   link: NavItem;
   isActive: boolean;
+  inView?: boolean;
+  delay: number;
 }
 
-export const NavLink: React.FC<NavLinkProps> = ({ link, isActive }) => {
+export const NavLink: React.FC<NavLinkProps> = ({ link, isActive, inView = false, delay }) => {
   return (
-    <Center h={{ base: '32px', md: '100%' }} pos="relative" role="group" pointerEvents={isActive ? 'none' : 'all'}>
-      <Link
-        href={`#${link.href}`}
-        h="100%"
-        borderRadius="full"
-        px={{ base: '32px', md: '8px' }}
-        fontWeight={isActive ? 'bold' : 'normal'}
-        color={isActive ? 'textPrimary.500' : 'textTertiary.500'}
-        bgGradient={{
-          base: isActive ? 'linear(to-r, peachOrange.500, white)' : 'white',
-          md: 'linear(to-r, white, white)'
-        }}
-        _groupHover={{
-          textDecor: 'none',
-          color: 'textSecondary.500'
-        }}
-        onClick={link.onClick}
-      >
-        <Center w="100%" h="100%">
-          {link.label}
-        </Center>
-      </Link>
+    <SlideFade in={inView} offsetX="100%" offsetY={0} transition={{ enter: { duration: 0.5, delay } }}>
+      <Center h={{ base: '32px', md: '100%' }} pos="relative" role="group" pointerEvents={isActive ? 'none' : 'all'}>
+        <Link
+          href={`#${link.href}`}
+          h="100%"
+          borderRadius="full"
+          px={{ base: '32px', md: '8px' }}
+          fontWeight={isActive ? 'bold' : 'normal'}
+          color={isActive ? 'textPrimary.500' : 'textTertiary.500'}
+          bgGradient={{
+            base: isActive ? 'linear(to-r, peachOrange.500, white)' : 'white',
+            md: 'linear(to-r, white, white)'
+          }}
+          _groupHover={{
+            textDecor: 'none',
+            color: 'textSecondary.500'
+          }}
+          onClick={link.onClick}
+        >
+          <Center w="100%" h="100%">
+            {link.label}
+          </Center>
+        </Link>
 
-      <Box
-        display={{ base: 'none', md: 'block' }}
-        w={!isActive ? '0%' : '80%'}
-        h="4px"
-        borderRadius="2px"
-        bgGradient="linear(to-r, peachRed.500, peachOrange.500)"
-        pos="absolute"
-        bottom="8px"
-        transition="0.2s"
-        _groupHover={{ w: '100%' }}
-      />
-    </Center>
+        <Box
+          display={{ base: 'none', md: 'block' }}
+          w={!isActive ? '0%' : '80%'}
+          h="4px"
+          borderRadius="2px"
+          bgGradient="linear(to-r, peachRed.500, peachOrange.500)"
+          pos="absolute"
+          bottom="8px"
+          transition="0.2s"
+          _groupHover={{ w: '100%' }}
+        />
+      </Center>
+    </SlideFade>
   );
 };
