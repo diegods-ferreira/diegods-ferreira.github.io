65fc810572ce49750eb5cea917dfb4bee500d553
Diego Ferreira <diegosf1705@gmail.com>
2022-07-30 14:32:03 -0300

    ContactPage created

---
+++ src/components/contact/social-media-card.component.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { IconType } from 'react-icons';
+import { FiGithub, FiGitlab, FiInstagram, FiLinkedin, FiTwitter } from 'react-icons/fi';
+import { Center, Icon, Link, SlideFade } from '@chakra-ui/react';
+
+interface SocialMediaCardProps {
+  type: 'linkedin' | 'github' | 'gitlab' | 'twitter' | 'instagram';
+  href: string;
+  inView?: boolean;
+  delay?: number;
+}
+
+const icons: Record<string, IconType> = {
+  linkedin: FiLinkedin,
+  github: FiGithub,
+  gitlab: FiGitlab,
+  twitter: FiTwitter,
+  instagram: FiInstagram
+};
+
+export const SocialMediaCard: React.FC<SocialMediaCardProps> = ({ type, href, inView = false, delay }) => {
+  return (
+    <SlideFade
+      in={inView}
+      transition={{ enter: { duration: 0.5, delay } }}
+      style={{
+        borderRadius: '16px',
+        boxShadow:
+          '-5px -5px 10px rgba(255, 255, 255, .05), 5px 5px 10px rgba(0, 0, 0, 0.3), inset -2px -2px 4px rgba(0, 0, 0, 0.1), inset 2px 2px 4px rgba(255, 255, 255, .05)'
+      }}
+      whileHover={{
+        scale: 1.1,
+        boxShadow:
+          '-5px -5px 15px rgba(255, 255, 255, .05), 5px 5px 15px rgba(0, 0, 0, 0.3), inset -2px -2px 4px rgba(0, 0, 0, 0.1), inset 2px 2px 4px rgba(255, 255, 255, .05)'
+      }}
+      whileTap={{
+        boxShadow: 'inset 6px 6px 10px 0 rgba(0, 0, 0, 0.2), inset -6px -6px 10px 0 rgba(255, 255, 255, .05)'
+      }}
+    >
+      <Link href={href} target="_blank" role="group">
+        <Center boxSize={{ base: '64px', md: '80px' }} borderRadius="16px">
+          <Icon
+            as={icons[type]}
+            boxSize={{ base: '20px', md: '24px' }}
+            color="textSecondary.300"
+            transition="0.5s"
+            _groupHover={{ color: 'textSecondary.50' }}
+          />
+        </Center>
+      </Link>
+    </SlideFade>
+  );
+};
--- src/pages/contact.page.tsx
+++ src/pages/contact.page.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { Center } from '@chakra-ui/react';
+import { Center, Heading, HStack, Image, ScaleFade, Text, VStack } from '@chakra-ui/react';
+
+import { SocialMediaCard } from '../components/contact/social-media-card.component';
+
+import profilePicture from '../assets/images/profile-picture.png';
 
 interface ContactPageProps {
   elementRef: any;
@@ -8,8 +12,35 @@ interface ContactPageProps {
 
 export const ContactPage: React.FC<ContactPageProps> = ({ elementRef, inView = false }) => {
   return (
-    <Center ref={elementRef} w="100%" bg="textSecondary.500" overflowX="hidden">
-      <h1 style={{ color: 'white' }}>ContactPage</h1>
+    <Center ref={elementRef} w="100%" bg="textPrimary.500" overflowX="hidden">
+      <VStack w="100%" maxW="1200px" px="24px" py={{ base: '64px', md: '80px' }} spacing="40px">
+        <ScaleFade initialScale={0} in={inView} transition={{ enter: { duration: 0.5 } }}>
+          <Heading color="textTertiary.500">Contato</Heading>
+        </ScaleFade>
+
+        <ScaleFade initialScale={0} in={inView} transition={{ enter: { duration: 0.5, delay: 0.5 } }}>
+          <Image src={profilePicture} alt="Diego Ferreira's profile picture" boxSize="200px" />
+        </ScaleFade>
+
+        <ScaleFade initialScale={0} in={inView} transition={{ enter: { duration: 0.5, delay: 1 } }}>
+          <Text color="textSecondary.50" fontSize={{ base: 'md', md: 'lg' }} align="center">
+            Você pode me contrar nas seguintes redes sociais
+          </Text>
+        </ScaleFade>
+
+        <HStack spacing="0" wrap="wrap" gap={{ base: '32px', md: '40px' }} justifyContent="center">
+          <SocialMediaCard type="github" href="https://github.com/diegods-ferreira" inView={inView} delay={1.5} />
+          <SocialMediaCard type="gitlab" href="https://gitlab.com/diegods-ferreira" inView={inView} delay={1.7} />
+          <SocialMediaCard type="instagram" href="https://www.instagram.com/diegodsf/" inView={inView} delay={1.9} />
+          <SocialMediaCard
+            type="linkedin"
+            href="https://www.linkedin.com/in/diego-de-souza-ferreira/"
+            inView={inView}
+            delay={2.1}
+          />
+          <SocialMediaCard type="twitter" href="https://twitter.com/diegoodsf" inView={inView} delay={2.3} />
+        </HStack>
+      </VStack>
     </Center>
   );
 };
