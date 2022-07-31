2cbac4096c308368efcfe0dc38f8ec6c2b377079
Diego Ferreira <diegosf1705@gmail.com>
2022-07-31 20:52:24 -0300

    Created personal profile section on about page

---
+++ src/components/about/personal-profile-item.component.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Center, ScaleFade, Text } from '@chakra-ui/react';
+
+interface PersonalProfileItemProps {
+  fontSize: string;
+  fontWeight: string;
+  inView?: boolean;
+  delay?: number;
+  children: React.ReactNode;
+}
+
+export const PersonalProfileItem: React.FC<PersonalProfileItemProps> = ({
+  fontSize,
+  fontWeight,
+  inView = false,
+  delay = 0,
+  children
+}) => {
+  return (
+    <ScaleFade
+      in={inView}
+      initialScale={0}
+      transition={{ enter: { duration: 0.5, delay } }}
+      style={{ width: '100%', height: '100%' }}
+    >
+      <Center boxSize="100%">
+        <Text
+          fontFamily="Montserrat"
+          fontSize={fontSize}
+          fontWeight={fontWeight}
+          borderRadius="16px"
+          cursor="pointer"
+          transition="0.2s"
+          _hover={{
+            transform: 'scale(1.05)',
+            textShadow: '0px 15px 5px rgba(0,0,0,0.1), 10px 20px 5px rgba(0,0,0,0.05), -10px 20px 5px rgba(0,0,0,0.05)'
+          }}
+        >
+          {children}
+        </Text>
+      </Center>
+    </ScaleFade>
+  );
+};
--- src/pages/about.page.tsx
+++ src/pages/about.page.tsx
@@ -1,13 +1,37 @@
 import React from 'react';
-import { Center, Stack, StackDivider, VStack } from '@chakra-ui/react';
+import { Center, Stack, StackDivider, VStack, Wrap, WrapItem } from '@chakra-ui/react';
 
 import { AboutItem } from '../components/about/about-item.component';
 import { AboutTitle } from '../components/about/about-title.component';
+import { PersonalProfileItem } from '../components/about/personal-profile-item.component';
 
 interface AboutPageProps {
   elementRef: any;
   inView?: boolean;
 }
+
+const PERSONAL_PROFILE_ITEM_FONT_STYLES: Record<number, Record<string, string>> = {
+  0: { size: '2xl', weight: 'light' },
+  1: { size: '2xl', weight: 'light' },
+  2: { size: '3xl', weight: 'normal' },
+  3: { size: '3xl', weight: 'normal' },
+  4: { size: '4xl', weight: 'bold' },
+  5: { size: '4xl', weight: 'bold' }
+};
+
+const getRandomNumberFromOneToFive = () => Math.round((Math.random() * 10) / 2);
+
+const getPersonalProfileItemFontStyles = () => PERSONAL_PROFILE_ITEM_FONT_STYLES[getRandomNumberFromOneToFive()];
+const getPersonalProfileItemDelay = () => 1 + getRandomNumberFromOneToFive() * 0.2;
+
+const personalProfileItems = [
+  { label: 'pró-ativo', fontStyles: getPersonalProfileItemFontStyles(), delay: getPersonalProfileItemDelay() },
+  { label: 'adaptável', fontStyles: getPersonalProfileItemFontStyles(), delay: getPersonalProfileItemDelay() },
+  { label: 'autodidata', fontStyles: getPersonalProfileItemFontStyles(), delay: getPersonalProfileItemDelay() },
+  { label: 'dedicado', fontStyles: getPersonalProfileItemFontStyles(), delay: getPersonalProfileItemDelay() },
+  { label: 'trabalho em equipe', fontStyles: getPersonalProfileItemFontStyles(), delay: getPersonalProfileItemDelay() },
+  { label: 'criativo', fontStyles: getPersonalProfileItemFontStyles(), delay: getPersonalProfileItemDelay() }
+];
 
 export const AboutPage: React.FC<AboutPageProps> = ({ elementRef, inView = false }) => {
   return (
@@ -46,6 +70,27 @@ export const AboutPage: React.FC<AboutPageProps> = ({ elementRef, inView = false
               No meu tempo livre gosto de jogar <i>videogames</i>, ler um bom livro ou conversar com amigos.
             </AboutItem>
           </VStack>
+        </Stack>
+
+        <Stack w="100%" direction={{ base: 'column', md: 'row' }} spacing="40px" alignItems="flex-start">
+          <AboutTitle inView={inView} delay={0.8}>
+            Perfil pessoal
+          </AboutTitle>
+
+          <Wrap justify="center" spacing="16px" overflow="visible">
+            {personalProfileItems.map((item) => (
+              <WrapItem key={item.label}>
+                <PersonalProfileItem
+                  fontSize={item.fontStyles.size}
+                  fontWeight={item.fontStyles.weight}
+                  inView={inView}
+                  delay={item.delay}
+                >
+                  {item.label}
+                </PersonalProfileItem>
+              </WrapItem>
+            ))}
+          </Wrap>
         </Stack>
       </VStack>
     </Center>
